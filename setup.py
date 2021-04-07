3840505ff2bd55dcef6a00ab8097ac9ad23e24a4
MrBin99 <paul.pages12@gmail.com>
2021-04-07 18:29:02 +0200

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from os import path
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
 
@@ -10,12 +10,14 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="django-vite",
-    version="1.0.1",
+    version="1.0.2",
     description="Integration of ViteJS in a Django project.",
     long_description=README,
     long_description_content_type="text/markdown",
     author="MrBin99",
     url="https://github.com/MrBin99/django-vite",
+    include_package_data=True,
+    packages=find_packages(),
     requires=[
         "Django (>=1.11)",
     ],
